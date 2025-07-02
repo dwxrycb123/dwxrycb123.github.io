86ec40617547be32ff7cd1d5881f0cad5492c626
dwxrycb123 <dwxrycb123@users.noreply.github.com>
2025-07-02 14:51:05 +0000

    Deploying to gh-pages from @ dwxrycb123/dwxrycb123.github.io@c11461274d71d8bf4b570f5847ebca2e67bec3b6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -89,7 +89,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%65%74%73%75%6E%61%69%6E%6E@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-github',
